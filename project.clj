c8cc8cb82fa0bbb949fa1c5b9ba0d9c33dab0c4b
Chris Ford <christophertford@gmail.com>
2013-08-03 13:03:45 +0300

    Standard project file.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject kraftwerk "0.1.0-SNAPSHOT"
+  :description "Covers of Kraftwerk tracks."
+  :url "http://github.com/ctford/kraftwerk"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [overtone "0.8.1"]
+                 [leipzig "0.7.0-SNAPSHOT"]])
