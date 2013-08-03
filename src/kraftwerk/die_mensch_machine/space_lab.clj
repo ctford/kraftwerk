fe4d3229fbef5007fd5d37b48e1f93ea62512ab3
Chris Ford <christophertford@gmail.com>
2013-08-03 14:39:41 +0300

    Tweak the delay on the first chorus, and echo throughout the verse.

--- src/kraftwerk/die_mensch_machine/space_lab.clj
+++ src/kraftwerk/die_mensch_machine/space_lab.clj
@@ -68,7 +68,10 @@
                      (then (phrase [1 1/2 5/2] [2 -1 0]))
                      (where :part (is :melody)))
         harmony (->> [7 6 5 4] (phrase (repeat 4)) (where :part (is :harmony)))
-        accompaniment (reduce with [bass harmony (times 2 beat)])]
+        echo (->> (concat (repeat 32 0) (repeat 14 3) (repeat 2 6) (repeat 16 0))
+                  (phrase (repeat 1/4))
+                  (where :part (is :echo)))
+        accompaniment (reduce with [bass harmony echo (times 2 beat)])]
     (->> (with melodya accompaniment)
          (then (with melodyb accompaniment)))))
 
@@ -76,7 +79,7 @@
   (->>
     intro
     (then (->> chorus (filter #(-> % :part #{:chords :melody}))))
-    (then chorus)
+    (then (after 4 chorus))
     (then verse)
     (wherever :pitch, :pitch (comp C minor)) 
     (where :duration (bpm 105)) 
