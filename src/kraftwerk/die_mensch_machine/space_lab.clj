f3313e60b0fa37f445220c0a9336f3af9d751021
Chris Ford <christophertford@gmail.com>
2013-08-03 14:12:01 +0300

    Now with chorus.

--- src/kraftwerk/die_mensch_machine/space_lab.clj
+++ src/kraftwerk/die_mensch_machine/space_lab.clj
@@ -24,6 +24,25 @@
     (with (->> (rhythm (repeat 8 1)) (having :drum (cycle [:kick :tock]))))
     (where :part (is :beat))))
 
+(def chorus
+  (let [chords (->> [nil (-> triad (root 2) (inv 1))
+                     nil (-> triad (root -4) (update-in [:iii] #(+ % 1/2)))]
+                    (phrase (cycle [1 3]))
+                    (where :part (is :chords)))
+        melodya  (->> [0 2 3 0] (phrase [1/2 7/2 1/2 8/2])
+                      (after -1/2)
+                      (where :part  (is :melody)))
+        melodyb  (->> melodya drop-last (then (phrase [8/2] [7]))
+                      (where :part (is :melody)))
+        harmony  (->> [7] (phrase [8]) (where :part (is :harmony)))
+        echo  (->> (concat (repeat 16 2) (repeat 16 0)) (phrase (repeat 1/4))
+                   (where :part (is :echo)))
+        accompaniment (reduce with [echo chords harmony beat])] 
+    (->> accompaniment
+         (then (with accompaniment melodya))
+         (then accompaniment)
+         (then (with accompaniment melodyb)))))
+         
 (def verse
   (let [bass (->> [0 2 3 6 7]
                   (phrase [7.5 0.5 3.5 0.5 4])
@@ -45,7 +64,8 @@
 
 (def track
   (->>
-    verse
+    chorus
+    (then verse)
     (wherever :pitch, :pitch (comp C minor)) 
     (where :duration (bpm 105)) 
     (where :time (bpm 105))))
@@ -95,6 +115,26 @@
         osc (overtone/saw freq)]
     (-> osc (overtone/lpf 1500) (* envelope) (* 1/2))))
 
+(overtone/definst res [freq 440]
+  (-> (overtone/perc 0 0.7)
+      (overtone/env-gen :action overtone/FREE)
+      (* (overtone/mix [(overtone/saw freq) (overtone/pulse (/ freq 2) 0.5)]))
+      (overtone/rlpf 800 0.1)
+      (* 1/2)))
+
+(overtone/definst poly [freq 440 dur 1000]
+  (let [envelope (overtone/env-gen (overtone/asr 0.2 1 0.1)
+                                   (overtone/line:kr 1.0 0.0 (/ dur 1000))
+                                   :action overtone/FREE)
+        level (overtone/env-gen (overtone/asr 7 1 0.2)
+                                :level-scale 12000)
+        osc1 (overtone/mix
+               (map
+                 #(overtone/pulse (* freq %) (+ 0.5 (* 0.3 (overtone/lf-cub 2))))
+                 [1 1.007]))
+        osc2 (overtone/pulse (/ freq 2) 0.3)]
+    (* 1/3 envelope (overtone/rlpf (+ osc1 osc2) level 0.9))))
+
 ; Arrangement
 (def kit {:kick bass-drum
           :tick hat
@@ -111,3 +151,13 @@
 
 (defmethod play-note :harmony [{midi :pitch ms :duration}]
   (-> midi overtone/midi->hz (* 2) (string ms))) 
+
+(defmethod play-note :echo [{midi :pitch}]
+  (-> midi overtone/midi->hz res))
+
+(defmethod play-note :chords [{midi :pitch ms :duration}]
+  (-> midi overtone/midi->hz (poly ms)))
+
+(defmethod play-note :default [note]
+  (print (str "No part for " note ". ")))
+
