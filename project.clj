4dead72194f0da93100577b19baa268cd2b1cacd
Chris Ford <christophertford@gmail.com>
2013-08-03 19:12:00 +0300

    Remove licensing because I don't want to imply I have the rights to the original song.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject kraftwerk "0.1.0-SNAPSHOT"
   :description "Covers of Kraftwerk tracks."
   :url "http://github.com/ctford/kraftwerk"
-  :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [overtone "0.8.1"]
                  [leipzig "0.7.0-SNAPSHOT"]])
