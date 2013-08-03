e70139027f91e5c900b1e16057f9ec80c1350385
Chris Ford <christophertford@gmail.com>
2013-08-03 13:24:09 +0300

    Hit the hat on the quarter notes.

--- src/kraftwerk/die_mensch_machine/space_lab.clj
+++ src/kraftwerk/die_mensch_machine/space_lab.clj
@@ -16,6 +16,16 @@
 
 (def inv inversion)
 
+; Notes
+(def track
+  (->>
+    (rhythm (repeat 32 1/4))
+    (where :part (is :beat))
+    (where :drum (is :tick))
+    (wherever :pitch, :pitch (comp C minor)) 
+    (where :duration (bpm 105)) 
+    (where :time (bpm 105))))
+
 ; Instruments
 (overtone/definst hat  [] 
   (-> (overtone/perc 0 0.05)
@@ -27,7 +37,13 @@
       (overtone/env-gen :action overtone/FREE)
       (* (overtone/mix [(overtone/sin-osc 200) (overtone/white-noise)]))))
 
-(overtone/definst kick  []
+(overtone/definst bass-drum []
   (-> (overtone/perc 0 0.05)
       (overtone/env-gen :action overtone/FREE)
       (* 3 (overtone/sin-osc 40))))
+
+; Arrangement
+(def kit {:kick bass-drum
+          :tick hat
+          :tock snare})
+(defmethod play-note :beat [{drum :drum}] ((drum kit)))
